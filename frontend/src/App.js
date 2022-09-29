9283130339d826ba18147c0c371b58909c13e35d
Brad Culley <culleyib@gmail.com>
2022-09-29 15:56:45 -0400

    Fixing bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import {useEffect} from "react";
 import axios from "axios";
 
 function App() {
-    useEffect(async () => {
-        try {
-            const result = await axios('/backend/make-call-that-uses-secret/brad/popcorn')
-            const {data} = result;
-            alert(data);
-        } catch (e) {
-            alert('failed to do the secret operation!');
-        }
-
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await axios('/backend/make-call-that-uses-secret/brad/popcorn')
+                const {data} = result;
+                alert(data);
+            } catch (e) {
+                alert('failed to do the secret operation!');
+            }
+        })();
     }, []);
 
   return (
